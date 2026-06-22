1c00cd0c8d39f8e926b2c8c8f1bed6ca6be5cfee
abhi <rajidiabhinavreddy@gmail.com>
2026-06-23 02:34:28 +0530

    learning routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Mainpage } from './Pages/mainpage/mainpage';
+import { CreateRoom } from './components/create-room/create-room';
+import { JoinRoom } from './components/join-room/join-room';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:Mainpage
+    },
+    {
+        path:'/create-room',
+        component:CreateRoom
+    },
+    {
+        path:'/join-room',
+        component:JoinRoom
+    }
+];
